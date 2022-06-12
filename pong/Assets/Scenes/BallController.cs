629dc10e085d3c15e74ff1673fa5724473bfdd19
Philip Hoktato <philiphoktato@gmail.com>
2022-06-12 14:48:42 +0700

    Base Gameplay
    2nd commit of Base Gameplay.
    Fixed:
    - Unactivated PaddleController script

--- pong/Assets/Scenes/BallController.cs
+++ pong/Assets/Scenes/BallController.cs
@@ -7,15 +7,16 @@ public class BallController : MonoBehaviour
     public Vector2 speed;
     private Rigidbody2D rig;
 
+    public Vector2 resetPosition;
+
     private void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         rig.velocity = speed;
     }
 
-    // Update is called once per frame
-    private void Update()
+    public void ResetBall()
     {
-        
+        transform.position = new Vector3(resetPosition.x, resetPosition.y, 1);
     }
 }
